d8c2f55dc393173784de2edb9620c8f26a7c3557
DUClement <du.clement@ensea.fr>
2023-12-08 11:05:11 +0100

    Update AuxiliaryTask partie batterie

--- Code/Core/Src/auxiliaryTask.c
+++ Code/Core/Src/auxiliaryTask.c
@@ -30,11 +30,6 @@
 #include "log/types.h"
 
 
-#define TASK_AUXILIARY_STACK_DEPTH 512
-#define TASK_AUXILIARY_PRIORITY 1
-#define BATTERY_SUPPLY 7.2
-#define BATTERY_THRESHOLD 6.4
-#define RESOLUTION_ADC 4096
 SemaphoreHandle_t xBatterySemaphore = NULL;
 TaskHandle_t h_task_batteryLevel = NULL;
 
@@ -45,12 +40,11 @@ void auxiliaryTask(){
 	float batteryLevel;
 
 	while (1) {
-		// état batterie
 		batteryLevel = getBatteryLevel();
-		// vérifie batterie
+		LOG_LED_DEBUG("Battery OK");
 		if (batteryLevel < BATTERY_THRESHOLD) {
 			xSemaphoreTake(xBatterySemaphore, portMAX_DELAY);
-			LOG_LED_DEBUG("Low battery\r\n");
+			LOG_LED_DEBUG("Low battery");
 			xSemaphoreGive(xBatterySemaphore);
 		}
 		vTaskDelay(1);
@@ -58,17 +52,22 @@ void auxiliaryTask(){
 }
 float getBatteryLevel() {
 	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	uint16_t adcValue = HAL_ADC_GetValue(&hadc1);
-	HAL_ADC_Stop(&hadc1);
-	float batteryLevel = (float)adcValue / RESOLUTION_ADC * BATTERY_SUPPLY;
+	float batteryLevel;
+	if(HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK){
+		uint16_t adcValue = HAL_ADC_GetValue(&hadc1);
+		HAL_ADC_Stop(&hadc1);
+		batteryLevel = (float)adcValue / RESOLUTION_ADC * BATTERY_SUPPLY;
+	}
+	else{
+		LOG_LED_ERROR("Get battery error");
+	}
 	return batteryLevel;
 }
 
 void createAuxiliaryTask() {
 	if 	(xTaskCreate(auxiliaryTask, "Auxiliary", TASK_AUXILIARY_STACK_DEPTH, NULL,
 			TASK_AUXILIARY_PRIORITY, &h_task_batteryLevel)!= pdPASS){
-		LOG_LED_ERROR("Error creating task auxiliary\r\n");
+		LOG_LED_ERROR("Error creating task auxiliary");
 
 	}
 	else
